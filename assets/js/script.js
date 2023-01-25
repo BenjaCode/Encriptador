13b029db3b73f36a2317d75292fcf094b7bbc12f
BenjaCode <lbenjajrcode@gmail.com>
2023-01-25 14:44:02 -0600

    Funcion para mostrar u ocultar los elementos del Resultado - script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 // ----Variables----
-let mensaje = document.querySelector('#tAMensaje');
-let btnEncriptar = document.querySelector('#btnEncriptar');
+const mensaje = document.querySelector('#tAMensaje');
+const btnEncriptar = document.querySelector('#btnEncriptar');
 const btnDesencriptar = document.querySelector('#btnDesencriptar');
+const imagenYTexto = document.querySelector('#imagenYTexto');
+const resultadoYBtn = document.querySelector('#resultadoYBtn')
 
 // ----Eventos click de los botones Mensaje----
 btnEncriptar.addEventListener('click', () => {
-    
+    ocultarImagenYTexto();
 });
 
 btnDesencriptar.addEventListener('click', () => {
     
 });
+
+// ----Funcion para mostrar u ocultar los elementos del Resultado
+function ocultarImagenYTexto() {
+    if(imagenYTexto.classList.value == 'ocultarImagenYTexto'){
+        resultadoYBtn.classList.add('ocultarImagenYTexto');
+        imagenYTexto.classList.remove('ocultarImagenYTexto');
+    }else{
+        imagenYTexto.classList.add('ocultarImagenYTexto');
+        resultadoYBtn.classList.remove('ocultarImagenYTexto');
+    }
+}
