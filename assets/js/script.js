9789c1f88e78fe563cc0e30a67fc30dbb22d15fb
BenjaCode <lbenjajrcode@gmail.com>
2023-01-25 14:20:31 -0600

    Eventos click de los botones Mensaje - script.js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// ----Variables----
+let mensaje = document.querySelector('#tAMensaje');
+let btnEncriptar = document.querySelector('#btnEncriptar');
+const btnDesencriptar = document.querySelector('#btnDesencriptar');
+
+// ----Eventos click de los botones Mensaje----
+btnEncriptar.addEventListener('click', () => {
+    
+});
+
+btnDesencriptar.addEventListener('click', () => {
+    
+});
