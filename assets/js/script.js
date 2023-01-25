4afe096e58c6f6773ebdaac67ecce11418bb86cc
BenjaCode <lbenjajrcode@gmail.com>
2023-01-25 15:57:40 -0600

    Funcion para encriptar el mensaje

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,16 @@ const btnEncriptar = document.querySelector('#btnEncriptar');
 const btnDesencriptar = document.querySelector('#btnDesencriptar');
 const imagenYTexto = document.querySelector('#imagenYTexto');
 const resultadoYBtn = document.querySelector('#resultadoYBtn');
+let tAResultado = document.querySelector('#tAResultado');
 
 // ----Eventos click de los botones Mensaje----
 btnEncriptar.addEventListener('click', () => {
     ocultarImagenYTexto();
+    encriptarMensaje();
 });
 
 btnDesencriptar.addEventListener('click', () => {
-    
+    ocultarImagenYTexto();
 });
 
 // ----Funcion para mostrar u ocultar los elementos del Resultado
@@ -23,4 +25,21 @@ function ocultarImagenYTexto() {
         imagenYTexto.classList.remove('ocultarImagenYTexto');
         resultadoYBtn.classList.add('ocultarImagenYTexto');
     }
+}
+
+// ----Funcion para encriptar el mensaje----
+function encriptarMensaje() {
+    let mensajeNormal = `${mensaje.value.toLowerCase()}`;
+
+    // /letra/ ES para indicar la letra a remplazar
+    // i = Tome en cuenta las letras mayusculas y minusculas
+    // m = Tome en cuenta multiples lineas
+    // g = Tome en cuenta todo el renglon o frase
+    let mensajeEncriptado = mensajeNormal.replace(/e/img, `enter`);
+    mensajeEncriptado = mensajeEncriptado.replace(/i/img, `imes`);
+    mensajeEncriptado = mensajeEncriptado.replace(/a/img, `ai`);
+    mensajeEncriptado = mensajeEncriptado.replace(/o/img, `ober`);
+    mensajeEncriptado = mensajeEncriptado.replace(/u/img, `ufat`);
+    
+    tAResultado.value = `${mensajeEncriptado}`;
 }
